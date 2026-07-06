5e4446874b9474c07b61faabc03ec47576a74b79
sreenithishm <sreenithish04@gmail.com>
2026-07-06 19:33:20 +0530

    solved leetcode 268 missing number

---
+++ src/Sorting/MissingNumber.java
@@ -0,0 +1,36 @@
+package Sorting;
+
+//268. Missing Number
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        int[] arr= {3,0,2,4,5};
+        System.out.println(cyclicSorting(arr));;
+    }
+    static int cyclicSorting(int[] arr){
+        int i =0;
+        while (i<arr.length){
+            int correct = arr[i];
+            if(arr[i]< arr.length &&arr[i]!=arr[arr[i]]){
+                swap(arr,i,correct);
+            }
+            else {
+                i++;
+            }
+        }
+        for (int j = 0; j < arr.length; j++) {
+            if(arr[j]!=j){
+                return j ;
+            }
+
+        }
+        return arr.length;
+    }
+    static void swap(int[] arr,int first,int second) {
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+
+    }
+
+}
