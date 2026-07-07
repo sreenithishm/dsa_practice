d8e669a3ffc7feeb71432a35a24d37a3eedecdfc
sreenithishm <sreenithish04@gmail.com>
2026-07-07 22:07:33 +0530

    solved LC 41 first missing positive

---
+++ src/Sorting/FirstMissingNum.java
@@ -0,0 +1,35 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class FirstMissingNum {
+    public static void main(String[] args) {
+        int[] arr={3,4,-1,1};
+        System.out.println(firstMissingPositive(arr));
+    }
+    public static int firstMissingPositive(int[] nums) {
+        int i =0;
+        while (i< nums.length){
+        int correct = nums[i]-1;
+        if(nums[i]>0&&nums[i]<= nums.length&&nums[i]!=nums[correct]){
+            swap(nums,i,correct);
+        }else {
+            i++;
+        }
+            System.out.println(Arrays.toString(nums));
+        }
+        for (int j = 0; j < nums.length; j++) {
+            if(nums[j]!=j+1){
+                return j+1;
+            }
+
+        }
+        return nums.length+1;
+    }
+
+    static void swap(int[] nums, int first, int second) {
+        int temp=nums[first];
+        nums[first]=nums[second];
+        nums[second]=temp;
+    }
+}
